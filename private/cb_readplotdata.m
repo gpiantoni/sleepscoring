23001ce5e835065036a369edbbd1bf6ba989428e
Gio <github@gpiantoni.com>
2012-07-27 09:00:27 +0200

    FIX: no epochs allowed before and after the recording

--- private/cb_readplotdata.m
+++ private/cb_readplotdata.m
@@ -6,11 +6,26 @@ opt = getappdata(0, 'opt');
 
 %-----------------%
 %-data to read
+%-------%
+%-between boundaries
+if opt.epoch < 1
+  opt.epoch = 1;
+end
+
+nepoch = floor(cfg.hdr.nSamples / cfg.hdr.Fs / cfg.wndw);
+if opt.epoch > nepoch
+  opt.epoch = nepoch;
+end
+%-------%
+
+%-------%
+%-update epoch info
 set(findobj('tag', 'epochnumber'), 'str', num2str(opt.epoch))
+setappdata(0, 'opt', opt);
+%-------%
 
 begsample = (opt.epoch - 1) * cfg.wndw * cfg.hdr.Fs + opt.recbegin * cfg.hdr.Fs;
 endsample = begsample + cfg.wndw * cfg.hdr.Fs - 1;
-%TODO: check it's not outside the limit
 %-----------------%
 
 dat = readdata(cfg, opt, begsample, endsample);
