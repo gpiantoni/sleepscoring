2d3dfc83f8fb0a69644b510f389e6f733880f1f0
Gio <github@gpiantoni.com>
2012-07-27 08:59:14 +0200

    ENH: elec name as in FASST now

--- preference/channame.m
+++ preference/channame.m
@@ -5,4 +5,27 @@ function label = channame(label)
 % stored in CFG.
 
 % Here one example of renaming one channel
+
+%-----------------%
+%-scalp EEG
+label{strcmp(label,  'E41')} = 'F3';
+label{strcmp(label, 'E214')} = 'F4';
+label{strcmp(label,  'E59')} = 'C3';
+label{strcmp(label, 'E183')} = 'C4';
+label{strcmp(label, 'E124')} = 'O3';
+label{strcmp(label, 'E149')} = 'O4';
+
+label{strcmp(label,  'E94')} = 'LM';
+label{strcmp(label, 'E190')} = 'RM';
+%-----------------%
+
+%-----------------%
+%-EOG
+label{strcmp(label, 'E234')} = 'REOG';
 label{strcmp(label, 'E244')} = 'LEOG';
+%-----------------%
+
+%-----------------%
+%-EMG
+label{strcmp(label, 'E165')} = 'EMG';
+%-----------------%
