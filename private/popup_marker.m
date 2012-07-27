f017e0a49b1e64742d5b50db57bd1b4d9e010b46
Gio <github@gpiantoni.com>
2012-07-27 09:04:42 +0200

    ENH: added marker and score popup

---
+++ private/popup_marker.m
@@ -0,0 +1,22 @@
+function popup_marker(cfg, opt)
+%---------------------------------------------------------%
+%-MAIN FUNCTION
+%---------------------------------------------------------%
+opt.i_marker = 1;
+uicontrol(findobj('tag', 'p_info'), 'sty', 'popup', 'uni', 'norm', ...
+  'pos', [.05 .85 .9 .1], 'str', opt.marker, 'val', opt.i_marker, 'tag', 'popupscore', ...
+  'call', @cb_marker);
+
+setappdata(0, 'opt', opt)
+%---------------------------------------------------------%
+
+
+%---------------------------------------------------------%
+%-CALLBACKS
+%---------------------------------------------------------%
+function cb_marker(h, eventdata)
+
+opt = getappdata(0, 'opt');
+opt.i_marker = get(h, 'val');
+setappdata(0, 'opt', opt)
+%---------------------------------------------------------%
---
+++ private/popup_score.m
@@ -0,0 +1,38 @@
+function popup_score(cfg, opt)
+%---------------------------------------------------------%
+%-MAIN FUNCTION
+%---------------------------------------------------------%
+if ~isempty(cfg.score)
+  
+  stages = {opt.stage.label};
+  score = cfg.score{1, cfg.rater}(opt.epoch);
+  i_score = find([opt.stage.code] == score);
+  if isempty(i_score)
+    i_score = find(isnan([opt.stage.code]));
+  end
+    
+  uicontrol(findobj('tag', 'p_info'), 'sty', 'popup', 'uni', 'norm', ...
+    'pos', [.05 .7 .9 .1], 'str', stages, 'val', i_score, 'tag', 'popupscore', ...
+    'call', @cb_score);
+  
+end
+%---------------------------------------------------------%
+
+
+%---------------------------------------------------------%
+%-CALLBACKS
+%---------------------------------------------------------%
+function cb_score(h, eventdata)
+
+cfg = getappdata(0, 'cfg');
+opt = getappdata(0, 'opt');
+
+i_score = get(h, 'val');
+cfg.score{1, cfg.rater}(opt.epoch) = opt.stage(i_score).code;
+
+opt.epoch = opt.epoch + 1;
+
+setappdata(0, 'cfg', cfg)
+setappdata(0, 'opt', opt)
+cb_readplotdata()
+%---------------------------------------------------------%
