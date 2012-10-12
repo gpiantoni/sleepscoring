148e2cde9826b089e5747fee4d1a1888b5b04201
Gio <github@gpiantoni.com>
2012-10-12 14:02:24 +0200

    ENH: make opt_ and meaningful name

--- preference/opt_micromed64.m
+++ preference/opt_micromed64.m
@@ -1,4 +1,4 @@
-function opt = opt_default
+function opt = opt_micromed64
 
 opt = [];
 
@@ -11,17 +11,7 @@ opt = [];
 % stored in CFG.
 %-----------------%
 %-scalp EEG
-opt.renamelabel = {'E41', 'F3';
-                  'E214', 'F4';
-                   'E59', 'C3';
-                  'E183', 'C4';
-                  'E124', 'O3';
-                  'E149', 'O4';
-                   'E94', 'LM'; % mastoid
-                  'E190', 'RM';
-                  'E234', 'REOG'; % EOG
-                  'E244', 'LEOG';
-                  'E165', 'EMG'};
+opt.renamelabel = {};
 %-----------------%
 %-------------------------------------%
 
@@ -48,52 +38,12 @@ opt.height.u = 1 - opt.marg.u - opt.marg.d; % height of objects above
 %-------------------------------------%
 %-HYPNOGRAM (cannot be modified in GUI)
 %-------------------------------------%
-%-code is the number saved in score{1,1}
-%-label is the corresponding state
-%-color is the color used for plotting
-%-height is the height of the bar
-stage = [];
-stage(1).code = 0;
-stage(1).label = 'Awake';
-stage(1).color = [0.2 0.75 0.6];
-stage(1).height = 7;
-
-stage(2).label = 'MT';
-stage(2).code = 6;
-stage(2).color = [0.9 0.4 0.4];
-stage(2).height = 6;
-
-stage(3).label = 'REM';
-stage(3).code = 5;
-stage(3).color = [0.1 0.5 0.9];
-stage(3).height = 5;
-
-stage(4).label = 'Stage 1';
-stage(4).code = 1;
-stage(4).color = [0.1 0.1 0.9];
-stage(4).height = 4;
-
-stage(5).label = 'Stage 2';
-stage(5).code = 2;
-stage(5).color = [0.1 0.1 0.75];
-stage(5).height = 3;
-
-stage(6).label = 'Stage 3';
-stage(6).code = 3;
-stage(6).color = [0.1 0.1 0.6];
-stage(6).height = 2;
-
-stage(7).label = 'Stage 4';
-stage(7).code = 4;
-stage(7).color = [0.1 0.1 0.45];
-stage(7).height = 1;
-
-stage(8).label = 'NOT SCORED';
-stage(8).code = NaN; % remember that == does not work with NaN
-stage(8).color = [1 1 1];
-stage(8).height = 0;
-
+% Choose scoring standard
+% hypnogram_RK for Rechtschaffen & Kales
+% hypnogram_aasm2007 for AASM 2007 (does not have "movement time" nor "stage 3"
+stage = hypnogram_aasm2007;
 opt.stage = stage;
+
 opt.hypnogrid = 30; % will create a grid every X minutes in hypnogram
 opt.arrowcolor = 'r'; % color of the arrow indicating the hypnogram
 %-------------------------------------%
@@ -158,23 +108,23 @@ opt.fft.i_chan = 1;
 %-------------------------------------%
 %-the same channel cannot belong to two groups at the same time
 opt.changrp(1).chantype = 'eeg'; % cannot be modified in GUI
-opt.changrp(1).chan = {'F3' 'F4' 'C3' 'C4' 'O3' 'O4'};  % can be modified in GUI
-opt.changrp(1).ref = {'LM' 'RM'};  % can be modified in GUI
+opt.changrp(1).chan = {'3' '4' '5' '6' '7' '8'};  % can be modified in GUI
+opt.changrp(1).ref = {'10' '11'};  % can be modified in GUI
 opt.changrp(1).Fhp = 0.3; % cannot be modified in GUI
 opt.changrp(1).Flp = 35; % cannot be modified in GUI
 opt.changrp(1).linecolor = 'k'; % cannot be modified in GUI
 opt.changrp(1).scaling = 1; % cannot be modified in GUI
 
 opt.changrp(2).chantype = 'eog';
-opt.changrp(2).chan = {'REOG'};
-opt.changrp(2).ref = {'LEOG'};
+opt.changrp(2).chan = {'12'};
+opt.changrp(2).ref = {'13'};
 opt.changrp(2).Fhp = 0.1;
 opt.changrp(2).Flp = 12;
 opt.changrp(2).linecolor = 'r';
 opt.changrp(2).scaling = 1;
 
 opt.changrp(3).chantype = 'emg';
-opt.changrp(3).chan = {'EMG'};
+opt.changrp(3).chan = {'14'};
 opt.changrp(3).ref = [];
 opt.changrp(3).Fhp = 0.3;
 opt.changrp(3).Flp = 35;
