3377b38ab2f6ac4d193247e67383b123465d2237
Gio <github@gpiantoni.com>
2013-02-28 13:06:24 +0100

    ENH: remove code, use label instead

--- preference/hypnogram_aasm2007.m
+++ preference/hypnogram_aasm2007.m
@@ -1,7 +1,6 @@
 function stage = hypnogram_aasm2007
 %HYPNOGRAM_AASM2007 Define stages according to AASM, 2007
 %
-%-code is the number saved in score{1,1}
 %-label is the corresponding state
 %-color is the color used for plotting
 %-height is the height of the bar
@@ -11,32 +10,27 @@ function stage = hypnogram_aasm2007
 % terminology, and technical specifications (Westchester, IL: American
 % Academy of Sleep Medicine).
 
-stage(1).code = 0;
-stage(1).label = 'Wakefulness';
-stage(1).color = [0.2 0.75 0.6];
-stage(1).height = 7;
+stage(1).label = 'NOT SCORED';
+stage(1).color = [1 1 1];
+stage(1).height = 0;
 
-stage(2).label = 'REM';
-stage(2).code = 5;
-stage(2).color = [0.1 0.5 0.9];
-stage(2).height = 4;
+stage(2).label = 'Wakefulness';
+stage(2).color = [0.2 0.75 0.6];
+stage(2).height = 7;
 
-stage(3).label = 'NREM 1';
-stage(3).code = 1;
-stage(3).color = [0.1 0.1 0.9];
-stage(3).height = 3;
+stage(3).label = 'REM';
+stage(3).color = [0.1 0.5 0.9];
+stage(3).height = 4;
 
-stage(4).label = 'NREM 2';
-stage(4).code = 2;
-stage(4).color = [0.1 0.1 0.75];
-stage(4).height = 2;
+stage(4).label = 'NREM 1';
+stage(4).color = [0.1 0.1 0.9];
+stage(4).height = 3;
 
-stage(5).label = 'NREM 3';
-stage(5).code = 3;
-stage(5).color = [0.1 0.1 0.45];
-stage(5).height = 1;
+stage(5).label = 'NREM 2';
+stage(5).color = [0.1 0.1 0.75];
+stage(5).height = 2;
 
-stage(6).label = 'NOT SCORED';
-stage(6).code = NaN; % remember that == does not work with NaN
-stage(6).color = [1 1 1];
-stage(6).height = 0;
+stage(6).label = 'NREM 3';
+stage(6).color = [0.1 0.1 0.45];
+stage(6).height = 1;
+
