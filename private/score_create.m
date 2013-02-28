ff1ca852dc4d69927e0ac7f4d22972f8f709d9ce
Gio <github@gpiantoni.com>
2013-02-28 12:57:47 +0100

    FIX: rater can be empty

--- private/score_create.m
+++ private/score_create.m
@@ -1,18 +1,21 @@
 function score = score_create(info, rater, wndw)
 %SCORE_CREATE: create empty score
-%-score structure, with fields
+% score structure, with fields
 %  .stage: score values (cell with names)
 %  .rater: name of the rater (string)
 %  .window: duration of scoring window (scalar, s)
-%  .score_beg: beginning of the scoring period (samples)
-%  .score_end: end of the scoring period (samples)
+%  .score_beg: beginning of the scoring period (s)
+%  .score_end: end of the scoring period (s)
 %  .marker:
 %    .name: name of marker (movement, arousal, artifact, etc)
 %    .time: 2xN matrix with artifacts (s)
+%
+% Called by
+%  - prepare_info
+%  - cb_rater
 
 %-------------------------------------%
 %-defaults
-if isempty(rater); rater = ''; end
 if isempty(wndw); wndw = 30; end
 %-------------------------------------%
 
@@ -20,12 +23,12 @@ if isempty(wndw); wndw = 30; end
 %-called by prepare_info, create empty structure
 score = [];
 
-score.rater = rater;
+score.rater = rater; % can be empty
 score.wdnw = wndw;
-score.score_beg = 1;
-score.score_end = info.hdr.nSamples;
+score.score_beg = 1 / info.fsample;
+score.score_end = info.hdr.nSamples / info.fsample;
 score.marker = [];
 
-nscore = floor(info.hdr.nSamples / info.fsample / wndw);
+nscore = floor((score.score_end - score.score_beg) / wndw);
 score.stage = cell(1, nscore);
 %-------------------------------------%
