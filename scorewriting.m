90c4e7fb60f98c43d9ecbfae6c17fdc34e83e3b9
Gio <github@gpiantoni.com>
2013-05-06 12:22:52 +0200

    ENH: you can specify the rater

--- scorewriting.m
+++ scorewriting.m
@@ -2,15 +2,19 @@ function scorewriting(varargin)
 %SCOREWRITING write the score for one rater to file
 %
 % Use as:
-%   scorestatistics(info) % one dataset
+%   scorewriting(info) % one dataset
 % or 
-%   scorestatistics(info1, info2) % multiple datasets
+%   scorewriting(info1, info2) % multiple datasets
 % or 
-%   scorestatistics('/path/to/sleepscoring.mat') % one dataset
+%   scorewriting('/path/to/sleepscoring.mat') % one dataset
 % or 
-%   scorestatistics('/path/to/sleepscoring.mat', '/path/to/csvfile.csv') % write to csvfile.csv
+%   scorewriting(info, '/path/to/csvfile.csv') % write to csvfile.csv
+% or 
+%   scorewriting(info, 'Jennifer') % score for specified rater,
+%                                       otherwise it uses the default
 %
-% It reports the scores in a table only for the rater specified in info.rater
+% You can pass the arguments in any order.
+% 
 % Before the table with scores, it reports the following information:
 %   - name of the mat file
 %   - name of the recording file
@@ -24,23 +28,25 @@ function scorewriting(varargin)
 %-------------------------------------%
 %-check input
 %-----------------%
-%-check if the last input is a text file or directory
+%-check if input contains csvfile or name of the rater
 tocsv = false;
-if ischar(varargin{end}) && ...
-    ~strcmp(varargin{end}(end-3:end), '.mat')
-  
-  tocsv = true;
-  csvfile = varargin{end};
-  
-  [~, ~, ext] = fileparts(csvfile);
-  if strcmp(ext, '')
-    fprintf('added extension ''csv''\n')
-    csvfile = [csvfile '.csv'];
-  end
-  
-  varargin(end) = [];
+rater_name = [];
 
-end
+is_csv = @(x) ischar(x) && strcmp(x(end-3:end), '.csv');
+where_csv = cellfun(is_csv, varargin);
+is_rater = @(x) ischar(x) && ~strcmp(x(end-3:end), '.mat');
+where_rater = cellfun(is_rater, varargin) & ~where_csv; % "& ~where_csv" means that we should not consider the csv file either
+
+if any(where_csv)
+  tocsv = true;
+  csvfile = varargin{where_csv};
+end  
+
+if any(where_rater)
+  rater_name = varargin{where_rater};
+end  
+
+varargin = varargin(~(where_csv | where_rater));
 %-----------------%
 
 %-----------------%
@@ -74,7 +80,7 @@ end
 %-------------------------------------%
 
 %-------------------------------------%
-%-prepare input
+%-prepare opt
 opt = prepare_opt(info.optfile);
 %-------------------------------------%
 
@@ -82,6 +88,22 @@ opt = prepare_opt(info.optfile);
 %-remove empty score, it's actually the default "not scored" usually
 for r = 1:numel(info.score)
   info.score(r).stage(cellfun(@isempty, info.score(r).stage)) = {opt.stage(1).label};
+end
+%-------------------------------------%
+
+%-------------------------------------%
+%-select rater if it exists
+if ~isempty(rater_name)
+  rater = find(strcmp({info.score.rater}, rater_name));
+  if isempty(rater)
+    rater = info.rater;
+    warning('could not find "%s" among raters %s. Using default "%s"', ...
+      rater_name, sprintf(' "%s"', info.score.rater), info.score(info.rater).rater);
+  end
+  
+else
+  rater = info.rater;
+  
 end
 %-------------------------------------%
 
@@ -102,11 +124,11 @@ end
 %-header info
 output = [info.infofile, sepa, info.dataset, sepa, ... 
   datestr(info.beginrec, 'dd-mmm-yyyy'), sepa, datestr(info.beginrec, 'HH:MM:SS'), sepa, ...
-  info.score(info.rater).rater, sepa, sprintf('% 3d', info.score(info.rater).wndw), '\n'];
+  info.score(rater).rater, sepa, sprintf('% 3d', info.score(info.rater).wndw), '\n'];
 %-----------------%
 
 %-----------------%
-score = info.score(info.rater);
+score = info.score(rater);
 
 for i = 1:numel(score.stage)
   epoch_beg = info.beginrec + (score.score_beg + (i - 1) * score.wndw) /60 /60 /24;
